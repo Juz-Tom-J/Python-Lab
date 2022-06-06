5eac45319e69c5cb85b1628f59da5693de1690bb
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-06-06 11:25:44 +0530

    Code added

--- Removing_Occ_No.py
+++ Removing_Occ_No.py
@@ -1,13 +1,13 @@
-n = int(input("Enter the number of elements: "))
-l = []
-input("Enter the elements: ")
+l=[]
+new_l=[]
+n = int(input("Enter the number of elements in the list : "))
+for i in range(0,n):
+    print("Enter the element",i+1,":",end = " ")
+    l.append(int(input()))
+print(l)
 
-for i in range(n):
-    l.append(int(input(" ")))
-
-rem = int(input("Enter the number you want to remove: "))
-
-while rem in l:
-    l.remove(rem)
-
-print("Updated list",l)
+r = int(input("Enter the element to be removed : "))
+for i in range(0,n):
+    if(l[i]!=r):
+        new_l.append(l[i])
+print("New List :",new_l)
