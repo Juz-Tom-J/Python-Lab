850a4da406de856397b24731308164bc2b04e353
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-07-02 09:50:32 +0530

    Code added

--- Dictionary.py
+++ Dictionary.py
@@ -1,11 +1,22 @@
-print("Enter the books: ")
-stock = {'Basics of Python': 50, 'Statistical Modelling':30}
-print("Original list: ",stock)
-up1 = {'Basics of Python': 60}
-stock.update(up1)
-print("Updated list: ",stock)
-remove = stock.popitem()
-print("Deleted item: ",remove)
-print("New List: ", stock)
-stock.update({'Probabilities and Statistics':55})
-print("Final List: ",stock)
+d1 ={"The Da Vinci Code": 5,"Wings Of Fire": 3,"The Alchemist": 4}
+print("\n1:Add stock\n2:Delete stock\n3:To add new book\n4:Exit")
+n=int(input("Enter your choice: "))
+while(n!=4):
+    if(n==1):
+        print(d1)
+        z=input("Enter the  book name: ")
+        y=int(input("Enter the no.of stocks you are adding: "))
+        d1[z]=d1[z]+y
+    elif(n==2):
+        print(d1)
+        z=input("Enter the book name: ")
+        y=int(input("Enter the no.of stocks you are deleting: "))
+        d1[z]=d1[z]-y
+    else:
+        print(d1)
+        z=input("Enter the book name: ")
+        u=int(input("Enter the no.of stocks: "))
+        d1.update({z:u})
+    print("\n1:Add stock\n2:Delete stock\n3:To add new book\n4:Exit")
+    n=int(input("Enter your choice:"))
+print(d1)
