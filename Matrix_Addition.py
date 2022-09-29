63d211d84fdd4b451aa4c6a5f352de50b2b869da
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2022-09-29 10:03:50 +0530

    Code updated

--- Matrix_Addition.py
+++ Matrix_Addition.py
@@ -1,33 +1,34 @@
-r = int(input("Enter the no. of rows: "))
-c = int(input("Enter the no. of columns: "))
-
-print("Matrix 1: ")
-m1 = []
-
-print("Enter the elements of matrix 1: ")
-for i in range(r):
-    x = []
-    for j in range(c):
-        x.append(int(input()))
-    m1.append(x)
-
-print("Matrix 2: ")
-m2 = []
-
-print("Enter the elements of matrix 2: ")
-for i in range(r):
-    y = []
-    for j in range(c):
-        y.append(int(input()))
-    m2.append(y)
-    
-res = [[0,0,0],
-       [0,0,0],
-       [0,0,0]]
-
-for i in range(len(m1)):
-    for j in range(len(m1[0])):
-        res[i][j] = m1[i][j]+m2[i][j]
-
-for r in res:
-    print(r)
+n = int(input("Enter N for N x N matrix: "))
+A = []
+print("Enter the element for Matrix 1: ")
+for i in range(n): 
+    row = []
+    for j in range(n):
+        row.append(int(input()))             
+    A.append(row)                              
+print(A)
+print("Display Array A In Matrix 1 Form")
+for i in range(n):
+    for j in range(n):
+        print(A[i][j], end=" ")
+    print() 
+B = []
+print("Enter the element for Matrix 2: ")
+for i in range(n): 
+    row = []                                      
+    for j in range(n):
+        row.append(int(input()))            
+    B.append(row)                            
+print(B)
+print("Display Array B In Matrix 2 Form")
+for i in range(n):
+    for j in range(n):
+        print(B[i][j], end=" ")
+    print()                                      
+result = [[0,0,0], [0,0,0], [0,0,0]] 
+for i in range(n):
+    for j in range(len(A[0])):
+        result[i][j] = A[i][j] + B[i][j] 
+print("Resultant Matrix is: ")
+for r in result: 
+    print(r) 
